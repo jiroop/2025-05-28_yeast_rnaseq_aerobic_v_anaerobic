a643c37cdfaab7e15738b2ccfbbb270a98b00de8
jiroop <jeremyianroop@gmail.com>
2025-05-28 19:51:20 -0700

    Analysis_finished_2025-05-28

---
+++ README.md
@@ -0,0 +1,39 @@
+##Yeast RNA-seq Analysis: Aerobic vs Anaerobic Growth Conditions
+
+This project analyzes RNA-seq data comparing yeast growth under aerobic and anaerobic conditions using a comprehensive bioinformatics pipeline. The entire analysis pipeline can be run by execution of the bash script ./scripts/pipeline.sh.
+
+Project Overview
+The pipeline performs differential gene expression analysis between yeast grown in aerobic versus anaerobic conditions, including:
+
+Quality control of raw sequencing reads
+Read mapping and quantification
+Differential expression analysis
+Data visualization and interpretation
+
+###Pipeline Workflow
+
+1. Data Download: Downloads sequencing reads and yeast transcriptome reference
+2. Quality Control: FastQC analysis of raw reads
+3. QC Summary: MultiQC report generation for FastQC results
+4. Read Mapping: Salmon quasi-mapping for transcript quantification
+5. Mapping QC: MultiQC report for Salmon mapping statistics
+6. Statistical Analysis: DESeq2 normalization and differential expression analysis in R
+
+
+
+###Installation
+
+1. Clone this repository
+
+cd 250518_yeast_anaerobic_v_aerobic_expression
+
+Create and activate the conda environment:
+
+conda env create -f environment.yml
+conda activate 250518_RNA-seq_yeast_anaerobic_v_aerobic
+
+Usage
+Run the complete pipeline:
+./scripts/pipeline.sh
+
+All steps in the pipeline and analysis are described in Analysis_Notebook.html
--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,29 +4,31 @@ SECONDS=0
 
 # STEP0 Set up directory stucture
 
-mkdir -p results/{differential_expression,salmon_mapping,qc/{fastqc_out,multiqc_out/{fastq,salmon_mapping}},visualization}
 mkdir -p data/{reference,fastq_raw}
-mkdir -p scripts
+mkdir -p mapped_reads_salmon analysis_plots scripts qc/{fastqc_out,multiqc_out/{fastqc,mapped_reads_salmon}} 
 
 
-# STEP1 Download fastq files
+## STEP1 Download fastq files
 
-wget -P ./data/fastq_raw https://figshare.com/ndownloader/articles/24182520/versions/2
-unzip ./data/fastq_raw/2 -d ./data/fastq_raw/ && rm ./data/fastq_raw/2
-mv ./data/fastq_raw/GCF_000146045.2_R64_rna.fna.gz ./data/reference && gunzip ./data/reference/GCF_000146045.2_R64_rna.fna.gz
+# wget -P ./data/fastq_raw https://figshare.com/ndownloader/articles/24182520/versions/2
+# unzip ./data/fastq_raw/2 -d ./data/fastq_raw/ && rm ./data/fastq_raw/2
+# rm ./data/fastq_raw/GCF_000146045.2_R64_rna.fna.gz 
 
 
-# STEP2 Run fastqc and MultiQC
+## STEP2 Run fastqc and MultiQC
 
-fastqc ./data/fastq_raw/*.gz -t 4 -o ./results/qc/fastqc_out/
-multiqc ./results/qc_control/fastqc_out -d -o ./results/qc_control/multiqc_out/fastq
+ fastqc ./data/fastq_raw/*.gz -t 4 -o qc/fastqc_out/
+ multiqc ./qc/fastqc_out -d -o ./qc/multiqc_out/fastqc
 
-# STEP3 Run Salmon to map reads
+## STEP3 Run Salmon to map reads
 
-# First, make the transcriptome index
-salmon index -t ./data/reference/GCF_000146045.2_R64_rna.fna -i ./data/reference/salmon_transcriptome_index --threads 4
+## First, get the cDNA references and make the transcriptome index
+wget -P data/reference/ https://ftp.ensemblgenomes.ebi.ac.uk/pub/fungi/release-61/fasta/saccharomyces_cerevisiae/cds/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz
 
-# # Now map the reads
+salmon index -t ./data/reference/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz -i ./data/reference/salmon_transcriptome_index --threads 4
+
+##  Now map the reads
+
 samples=("aerobic_r1" "aerobic_r2" "anaerobic_r1" "anaerobic_r2")
 
 for sample in "${samples[@]}"; do
@@ -34,17 +36,26 @@ for sample in "${samples[@]}"; do
 		-l A \
 		-1 data/fastq_raw/${sample}_1.fq.gz \
 		-2 data/fastq_raw/${sample}_2.fq.gz \
-		-o results/salmon_mapping/${sample} \
+		-o mapped_reads_salmon/${sample} \
 		--threads 4 \
 		--validateMappings 
 done
 
-# STEP4 Run MultiQC on the mapping files
+## STEP4 Run MultiQC on the mapping files
 
-multiqc -d results/salmon_mapping \
-	--outdir results/qc/multiqc_out/salmon_mapping \
-	--title "Salmon Mapping QC" \
-	--filename salmon_mapping_multiqc_report
+ multiqc -d mapped_reads_salmon \
+ 	--outdir qc/multiqc_out/mapped_reads_salmon \
+ 	--title "mapped_reads_salmon QC" \
+ 	--filename mapped_reads_salmon_multiqc_report
+
+
+# STEP5 Run Analysis_Notebook.qmd to finish analysis. See Notebook for details.
+wget -P ./data/reference/ http://sgd-archive.yeastgenome.org/curation/literature/go_slim_mapping.tab
+
+quarto render ../Analysis_Notebook.qmd
+
+
+
 
 
 
